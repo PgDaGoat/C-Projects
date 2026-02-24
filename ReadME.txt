58b6fcd812eaf619b855bd3bded979a1d5499391
PG <130524387+PgDaGoat@users.noreply.github.com>
2026-02-24 17:03:21 -0600

    new

--- ReadME.txt
+++ ReadME.txt
@@ -1,1 +1,11 @@
 Welcome to the world of C++!
+
+Here I will update what topics I am studying each day:
+
+2/20/2026
+*Statements and structure of a program.
+2/21/2026
+*Comments
+*Introduction to Objects and variables
+*Variable Assignment and Initialization
+*Introduction to iostream: cout, cin, and endl
---
+++ variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    int width;
+    width = 6;
+    int y;
+    y = 7;
+    int x {8}; //defined as an integer and has been initialized with the value of 5
+    std::cout << width;
+    std::cout << y;
+    std::cout << x;
+    //Below are the different types of initialization
+    int a; //default-initialization (no initializer)
+    //Traditional initialization forms:
+    int b = 5; //copy-initialization (initial value after equals sign)
+    int c(6); //direct-initilization (initial value in parenthesis)
+    //Modern Initialization forms (preferred):
+    int d {7}; //direct list initialization (initial value in braces)
+    int e{}; //value initialization (empty braces)
+    return 0;
+    
+}
